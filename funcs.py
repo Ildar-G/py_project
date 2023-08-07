a73e7310b3c1e00f539ea3bbecaf55bf65219aea
ildar-G <igazimov83@gmail.com>
2023-08-07 21:09:47 +0300

    create func subtraction

--- funcs.py
+++ funcs.py
@@ -2,3 +2,8 @@ def addition():
     a = int(input('Введите первое число ' ))
     b = int(input('Введите второе число ' ))
     print('Результат - {a + b}')
+
+    def subtraction():
+        a = int(input('Введите первое число '))
+        b = int(input('Введите второе число '))
+        print('Результат - {a - b}')
