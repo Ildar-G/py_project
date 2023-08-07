d20e87a56ae35a526e39054d4346cd7578fabed6
ildar-G <igazimov83@gmail.com>
2023-08-07 21:22:18 +0300

    import func addition()

--- funcs.py
+++ funcs.py
@@ -1,9 +1,9 @@
 def addition():
     a = int(input('Введите первое число ' ))
     b = int(input('Введите второе число ' ))
-    print('Результат - {a + b}')
+    print(f'Результат - {a + b}')
 
-    def subtraction():
-        a = int(input('Введите первое число '))
-        b = int(input('Введите второе число '))
-        print('Результат - {a - b}')
+def subtraction():
+    a = int(input('Введите первое число '))
+    b = int(input('Введите второе число '))
+    print(f'Результат - {a - b}')
